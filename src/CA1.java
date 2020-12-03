d0a38e79ddd842d1bfd90bff918b006656f804de
YWR <yuri.ribeiro@gmail.com>
2020-12-03 11:29:12 +0000

    2 commit

--- src/CA1.java
+++ src/CA1.java
@@ -6,13 +6,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 
-/*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
- *
- * @author Yuri
- */
+ //@author Yuri
+
 public class CA1 {
 
     /**
